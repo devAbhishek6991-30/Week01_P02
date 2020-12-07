4cebb2a1516df0a6e1d511310b635cff39b340e4
Abhishek Mishra <abhishek6991.30@gmail.com>
2020-12-07 10:54:13 +0530

    Created a bumpy signal for road and tested properly. The name of signal is x.

---
+++ bump_road.m
@@ -0,0 +1,12 @@
+Fs = 512;                   % samples per second
+   dt = 1/Fs;                   % seconds per sample
+   StopTime = 0.25;             % seconds
+   t = (0:dt:StopTime-dt)';     % seconds
+   %%Sine wave:
+   Fc = 10;                     % hertz
+   x = 0.02*sin(2*pi*Fc*t);
+   % Plot the signal versus time:
+   figure;
+   plot(t,x);
+   xlabel('time (in seconds)');
+   title('Signal versus Time');
